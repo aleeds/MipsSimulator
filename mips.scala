825cbf3a1fc4b3659bcf4732e18a2c8e3c5f6640
Andy Leeds <aleeds444@gmail.com>
2016-04-21 13:38:32 -0500

    Has the base class now

---
+++ mips.scala
@@ -0,0 +1,7 @@
+// Main class. Will perform execution, will store the data that will get changed
+// Will run everything. 
+object mips {
+  def main(args : Array[String]): Unit = {
+    println("Hello world");
+  }
+}
